34bc777fcf8d7f5dfb83bc9c89422aecfb80b990
Try Wijono <3wijono@gmail.com>
2021-01-04 23:45:51 +0800

    add users router and controller

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const user_controller = require("../controller/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/log-in", (req, res, next) => {
+  res.send("login");
+});
+
+router.get("/sign-up", (req, res, next) => {
+  res.send("sign-up");
+});
+
+router.post("/sign-up", (req, res, next) => {
+  res.send("sign-up post");
+});
+
+router.get("/log-off", (req, res, next) => {
+  res.send("logoff");
 });
 
 module.exports = router;
