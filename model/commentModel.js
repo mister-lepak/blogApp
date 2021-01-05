74a25c2c2e107dd5b7f03f0b2ec953e75659f0e6
Try Wijono <3wijono@gmail.com>
2021-01-05 08:04:12 +0800

    add luxon and udd timestamp info into comment and post model

--- model/commentModel.js
+++ model/commentModel.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -5,6 +6,17 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
   content: { type: String, required: true },
   published: { type: Boolean, required: true },
+  timeStamp: { type: Date, default: Date.now },
+});
+
+CommentSchema.virtual("url").get(() => {
+  return "/comment/" + this._id;
+});
+
+CommentSchema.virtual("timeStampFormatted").get(() => {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(
+    DateTime.DATE_MED_WITH_WEEKDAY
+  );
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
--- model/postModel.js
+++ model/postModel.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const PostSchema = new Schema({
   name: { type: String, required: true, maxlength: 50 },
   content: { type: String, required: true },
   published: { type: Boolean, required: true },
+  timeStamp: { type: Date, default: Date.now },
+});
+
+PostSchema.virtual("url").get(() => {
+  return "/post/" + this._id;
+});
+
+PostSchema.virtual("timeStampFormatted").get(() => {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(
+    DateTime.DATETIME_MED_WITH_WEEKDAY
+  );
 });
 
 module.exports = mongoose.model("Post", PostSchema);
