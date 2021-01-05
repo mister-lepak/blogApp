b8c8493bc83cb74dd454ba7c2fad316b9724a112
Try Wijono <3wijono@gmail.com>
2021-01-05 08:12:31 +0800

    add routers for post

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,36 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/posts", (req, res, next) => {
+  res.send("blog posts list");
+});
+
+router.get("/post/create", (req, res, next) => {
+  res.send("blog create get");
+});
+
+router.post("/post/create", (req, res, next) => {
+  res.send("blog create post");
+});
+
+router.get("/post/:id", (req, res, next) => {
+  res.send("blog post detail");
+});
+
+router.get("/post/:id/delete", (req, res, next) => {
+  res.send("blog delete get");
+});
+
+router.post("/post/:id/delete", (req, res, next) => {
+  res.send("blog delete post");
+});
+
+router.get("/post/:id/update", (req, res, next) => {
+  res.send("blog update get");
+});
+
+router.post("/post/:id/update", (req, res, next) => {
+  res.send("blog update post");
+});
+
 module.exports = router;
