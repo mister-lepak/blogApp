8bf9d70099b3b96ea6acaab1f47fe589f570c18f
Try Wijono <3wijono@gmail.com>
2021-01-06 08:58:44 +0800

    add indext postcontroller

--- controller/postController.js
+++ controller/postController.js
@@ -1,1 +1,18 @@
 const Post = require("../model/postModel");
+require("../model/userModel");
+require("../model/commentModel");
+const async = require("async");
+
+exports.index = (req, res, next) => {
+  async.parallel(
+    {
+      posts: (callback) => {
+        Post.find().populate("comment").populate("user").exec(callback);
+      },
+    },
+    (err, result) => {
+      if (err) return next(err);
+      res.json(result.posts);
+    }
+  );
+};
